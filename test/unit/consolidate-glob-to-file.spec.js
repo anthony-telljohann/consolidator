d595f5df25bc0e72d1c30cf0073aa6c16881ce49
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-08-31 02:02:03 -0400

    win so hard bed time

--- test/unit/consolidate-glob-to-file.spec.js
+++ test/unit/consolidate-glob-to-file.spec.js
@@ -1,70 +1,37 @@
 import { consolidateGlobToFile } from 'consolidator'
 import { SOURCES, DESTINATION } from 'constants'
+import assert from './assertions.js'
 
 const SOURCES_SHOULD_BE_NON_EMPTY_STRING = `sources should be a non-empty string`
 const DESTINATION_SHOULD_BE_NON_EMPTY_STRING = `destination should be a non-empty string`
 
 describe(`consolidateGlobToFile`, () => {
-  it(`should be a function`, async () => {
-    consolidateGlobToFile.should.be.a.Function()
+  beforeEach(async function() {
+    this.context = consolidateGlobToFile
   })
-
-  describe(`not specifying sources and not specifying destination`, () => {
-    it(`should be rejected`, async () => {
-      consolidateGlobToFile().should.be.rejected()
+  assert.shouldBeAFunction()
+  describe(`not specifying source and not specifying destination`, () => {
+    beforeEach(async function() {
+      this.context = consolidateGlobToFile()
     })
-    it(`should be rejected with TypeError`, async () => {
-      consolidateGlobToFile().should.be.rejectedWith(TypeError)
-    })
-    it(`should be rejected with "${SOURCES_SHOULD_BE_NON_EMPTY_STRING}"`, async () => {
-      consolidateGlobToFile().should.be.rejectedWith(
-        SOURCES_SHOULD_BE_NON_EMPTY_STRING
-      )
-    })
+    assert.shouldBeRejected(SOURCES_SHOULD_BE_NON_EMPTY_STRING)
   })
-
   describe(`not specifying sources and specifying destination as "${DESTINATION.FILE}"`, () => {
-    it(`should be rejected`, async () => {
-      consolidateGlobToFile(undefined, DESTINATION.FILE).should.be.rejected()
+    beforeEach(async function() {
+      this.context = consolidateGlobToFile(undefined, DESTINATION.FILE)
     })
-    it(`should be rejected with TypeError`, async () => {
-      consolidateGlobToFile(undefined, DESTINATION.FILE).should.be.rejectedWith(
-        TypeError
-      )
-    })
-    it(`should be rejected with "${SOURCES_SHOULD_BE_NON_EMPTY_STRING}"`, async () => {
-      consolidateGlobToFile(undefined, DESTINATION.FILE).should.be.rejectedWith(
-        SOURCES_SHOULD_BE_NON_EMPTY_STRING
-      )
-    })
+    assert.shouldBeRejected(SOURCES_SHOULD_BE_NON_EMPTY_STRING)
   })
   describe(`specifying sources as "${SOURCES.GLOB}" and not specifying destination`, () => {
-    it(`should be rejected`, async () => {
-      consolidateGlobToFile(SOURCES.GLOB, undefined).should.be.rejected()
+    beforeEach(async function() {
+      this.context = consolidateGlobToFile(SOURCES.GLOB, undefined)
     })
-    it(`should be rejected with TypeError`, async () => {
-      consolidateGlobToFile(SOURCES.GLOB, undefined).should.be.rejectedWith(
-        TypeError
-      )
-    })
-    it(`should be rejected with "${DESTINATION_SHOULD_BE_NON_EMPTY_STRING}"`, async () => {
-      consolidateGlobToFile(SOURCES.GLOB, undefined).should.be.rejectedWith(
-        DESTINATION_SHOULD_BE_NON_EMPTY_STRING
-      )
-    })
+    assert.shouldBeRejected(DESTINATION_SHOULD_BE_NON_EMPTY_STRING)
   })
   describe(`specifying sources as "${SOURCES.GLOB}" and specifying destination as "${DESTINATION.FILE}"`, () => {
-    it(`should be fulfilled`, async () => {
-      consolidateGlobToFile(
-        SOURCES.GLOB,
-        DESTINATION.FILE
-      ).should.be.fulfilled()
+    beforeEach(async function() {
+      this.context = consolidateGlobToFile(SOURCES.GLOB, DESTINATION.FILE)
     })
-    it(`should be fulfilled with undefined`, async () => {
-      consolidateGlobToFile(
-        SOURCES.GLOB,
-        DESTINATION.FILE
-      ).should.be.fulfilledWith(undefined)
-    })
+    assert.shouldBeFulfilled()
   })
 })

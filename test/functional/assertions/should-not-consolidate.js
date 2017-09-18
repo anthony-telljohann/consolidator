a0a53431e49a6f848501ba2e19fef8c82c780409
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-09-18 18:30:53 -0400

    format all the things

--- test/functional/assertions/should-not-consolidate.js
+++ test/functional/assertions/should-not-consolidate.js
@@ -1,9 +1,9 @@
-export default function shouldNotConsolidate () {
-  it(`should not create a destination file`, async function () {
+export default function shouldNotConsolidate() {
+  it(`should not create a destination file`, async function() {
     this.after.destinationFile.exists.should.be.false()
   })
 
-  it(`should remove every source file`, async function () {
+  it(`should remove every source file`, async function() {
     this.after.sourceFiles.forEach(sourceFile => {
       sourceFile.exists.should.be.false()
     })
--- test/functional/classes/random-file.js
+++ test/functional/classes/random-file.js
@@ -3,7 +3,7 @@ import path from 'path'
 import randomatic from 'randomatic'
 
 export default class RandomFile extends File {
-  constructor (directory) {
+  constructor(directory) {
     super(path.join(directory, randomatic('A', 10)))
   }
 }

c79b736ce2809cab4d67fe02f61f01cfc7d2d3f9
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-09-18 22:07:08 -0400

    squash

---
+++ test/functional/assertions/index.js
@@ -0,0 +1,7 @@
+import shouldConsolidate from './should-consolidate.js'
+import shouldNotConsolidate from './should-not-consolidate.js'
+
+export default {
+  shouldConsolidate,
+  shouldNotConsolidate
+}

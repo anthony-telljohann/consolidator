9e6dd6a7acae0f01db5c6301bcdae2b97d7e0731
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-11-18 10:56:06 -0500

    Update should-be-fulfilled.js

--- test/unit/assertions/should-be-fulfilled.js
+++ test/unit/assertions/should-be-fulfilled.js
@@ -1,8 +1,8 @@
 export default function shouldBeFulfilled() {
   it(`should be fulfilled`, async function() {
-    this.context.should.be.fulfilled()
+    this.context().should.be.fulfilled()
   })
   it(`should be fulfilled with undefined`, async function() {
-    this.context.should.be.fulfilledWith(undefined)
+    this.context().should.be.fulfilledWith(undefined)
   })
 }

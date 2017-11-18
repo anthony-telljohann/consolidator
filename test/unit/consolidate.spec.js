88d0f96638e2d7ce0d0286ae98a5558f6b60dfdd
Anthony Telljohann-Smothers <anthony@telljohann-smothers.com>
2017-11-18 10:51:55 -0500

    Update consolidate.spec.js

--- test/unit/consolidate.spec.js
+++ test/unit/consolidate.spec.js
@@ -4,42 +4,45 @@ import assert from 'unit/assertions'
 
 describe(`consolidate`, () => {
   beforeEach(async function() {
-    this.context = consolidate
+    this.sources = undefined
+    this.destination = undefined
+    this.context = function() {
+      return consolidate(this.sources, this.destination)
+    }
   })
   assert.shouldBeAFunction()
   describe(`not specifying source and not specifying destination`, () => {
-    beforeEach(async function() {
-      this.context = consolidate()
-    })
     assert.shouldBeRejected(SOURCES.SHOULD_BE_AN_ARRAY_OR_A_NON_EMPTY_STRING)
   })
   describe(`not specifying sources and specifying destination as "${DESTINATION.FILE}"`, () => {
     beforeEach(async function() {
-      this.context = consolidate(undefined, DESTINATION.FILE)
+      this.destination = DESTINATION.FILE
     })
     assert.shouldBeRejected(SOURCES.SHOULD_BE_AN_ARRAY_OR_A_NON_EMPTY_STRING)
   })
   describe(`specifying sources as [${SOURCE.FILES}] and not specifying destination`, () => {
     beforeEach(async function() {
-      this.context = consolidate(SOURCE.FILES, undefined)
+      this.sources = SOURCE.FILES
     })
     assert.shouldBeRejected(DESTINATION.SHOULD_BE_NON_EMPTY_STRING)
   })
   describe(`specifying sources as "${SOURCES.GLOB}" and not specifying destination`, () => {
     beforeEach(async function() {
-      this.context = consolidate(SOURCES.GLOB, undefined)
+      this.sources = SOURCES.GLOB
     })
     assert.shouldBeRejected(DESTINATION.SHOULD_BE_NON_EMPTY_STRING)
   })
   describe(`specifying sources as [${SOURCE.FILES}] and specifying destination as "${DESTINATION.FILE}"`, () => {
     beforeEach(async function() {
-      this.context = consolidate(SOURCE.FILES, DESTINATION.FILE)
+      this.sources = SOURCE.FILES
+      this.destination = DESTINATION.FILE
     })
     assert.shouldBeFulfilled()
   })
   describe(`specifying sources as "${SOURCES.GLOB}" and specifying destination as "${DESTINATION.FILE}"`, () => {
     beforeEach(async function() {
-      this.context = consolidate(SOURCES.GLOB, DESTINATION.FILE)
+      this.sources = SOURCE.GLOB
+      this.destination = DESTINATION.FILE
     })
     assert.shouldBeFulfilled()
   })
